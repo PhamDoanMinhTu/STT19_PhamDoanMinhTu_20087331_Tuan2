adb175a034b10ebbb0fe8d92a7a7e7db17595cd2
PhamDoanMinhTu <144188315+PhamDoanMinhTu@users.noreply.github.com>
2023-09-13 08:53:03 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+//const urlBacon ="https://baconipsum.com/api/?type=all-meat";
+
+//Jquery Ajax
+const urlBacon = "https://jsonplaceholder.typicode.com/todos/1";
+
+(function() {
+    $.ajax({
+
+        method: "GET",
+        url: urlBacon,
+        dataType: "json"
+
+    })
+
+    .done(function(data) {
+
+        console.log(data);
+
+    })
+
+    .fail(function() {
+
+        alert("no good");
+
+    });
+
+})()
+
+
+
+// /XMLHttpRequest RESTful GET
